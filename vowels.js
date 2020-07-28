9da35a22708cbcf4083561f482873b74ca298eb6
akhiln95 <akhilnalliboina@gmail.com>
2020-07-28 12:46:16 +0200

    vowels_update

--- vowels.js
+++ vowels.js
@@ -3,7 +3,7 @@ function findVowels(abc) {
     var count=0;
     var checker= ['a','e','i','o','u']
   for(var car of abc.toLowerCase()){
-     if(car.includes(checker)){
+     if(checker.includes(car)){
          count++;
      }
 return console.log("vowel count is: ", count)
