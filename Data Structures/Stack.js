6c4792a49f4fa195fcf55837affe28cbe29f8be8
akhiln95 <akhilnalliboina@gmail.com>
2021-03-16 13:48:21 +0100

    Stack implementation

--- Data Structures/Stack.js
+++ Data Structures/Stack.js
@@ -18,3 +18,15 @@ class Stack {
     return this.length == 0;
   }
 }
+
+// Imagine you’re building a navigation component for a product website we’ll call “Rainforest”. Users can navigate from the home page to different product pages within the site. For example, a user flow might be Home > Kitchen > Small Appliances > Toasters.
+//Build a navigation component that displays the list of previously visited pages as well as a back button which lets the user go back to the previous page.
+
+stack = new Stack();
+stack.push("Home");
+stack.push("Kitchen");
+stack.push("Bathroom");
+stack.peek();
+stack.pop("Bathroom");
+stack.isEmpty();
+stack.length;
