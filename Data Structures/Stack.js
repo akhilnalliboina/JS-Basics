3bfbfb06a1225c656d024e2ee6b50afeb4fa5dfd
akhiln95 <akhilnalliboina@gmail.com>
2021-03-31 22:40:14 +0200

    stack example

--- Data Structures/Stack.js
+++ Data Structures/Stack.js
@@ -19,9 +19,6 @@ class Stack {
   }
 }
 
-// Imagine you’re building a navigation component for a product website we’ll call “Rainforest”. Users can navigate from the home page to different product pages within the site. For example, a user flow might be Home > Kitchen > Small Appliances > Toasters.
-//Build a navigation component that displays the list of previously visited pages as well as a back button which lets the user go back to the previous page.
-
 stack = new Stack();
 stack.push("Home");
 stack.push("Kitchen");
